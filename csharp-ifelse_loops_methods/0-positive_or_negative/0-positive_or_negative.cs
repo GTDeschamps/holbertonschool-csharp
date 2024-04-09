c7bdab92cdc38d477d5be47d9d560d1fd39cc6c6
GTDeschamps <g.deschamps@hotmail.fr>
2024-04-09 15:55:00 +0200

    correction of Task 0

--- csharp-ifelse_loops_methods/0-positive_or_negative/0-positive_or_negative.cs
+++ csharp-ifelse_loops_methods/0-positive_or_negative/0-positive_or_negative.cs
@@ -7,17 +7,17 @@ class Program
 		Random rndm = new Random();
 		int number = rndm.Next(-10, 10);
 
-		if (number > 0);
+		if (number > 0)
 		{
-			Console.Writeline ($"{number} is positive")
+			Console.Writeline ($"{number} is positive");
 		}
-		else if (number < 0);
+		else if (number < 0)
 		{
-			Console.Whriteline ($"{number} is negative")
+			Console.Whriteline ($"{number} is negative");
 		}
 		else
 		{
-			Console.Whriteline ($"{number} is zero")
+			Console.Whriteline ($"{number} is zero");
 		}
 	}
 }
