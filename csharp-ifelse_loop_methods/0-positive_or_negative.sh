1edf2822f2e1dd87fb92e5f9867e57cd948f9eb9
GTDeschamps <g.deschamps@hotmail.fr>
2024-04-09 15:36:06 +0200

    Task 0: positive or negative

---
+++ csharp-ifelse_loop_methods/0-positive_or_negative.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+dotnet new console -o 0-positive_or_negative
+dotnet build 0-positive_or_negative
+cd 0-positive_or_negative
+dotnet run
---
+++ csharp-ifelse_loop_methods/0-positive_or_negative/0-positive_or_negative.cs
@@ -0,0 +1,22 @@
+﻿﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rndm = new Random();
+        int number = rndm.Next(-10, 10);
+	if (number > 0);
+		{
+			Console.Writeline ($"{number} is positive")
+		}
+	else if (number < 0);
+	{
+			Console.Whriteline ($"{number} is negative")
+	}
+	else
+	{
+		Console.Whriteline ($"{number} is zero")
+	}
+    }
+}
