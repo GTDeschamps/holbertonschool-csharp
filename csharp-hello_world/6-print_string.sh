58fca3dbb084a19187a73d405160bd2e1e5321fc
GTDeschamps <g.deschamps@hotmail.fr>
2024-04-09 10:59:29 +0200

    Task 6: print string

---
+++ csharp-hello_world/6-print_string.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+dotnet new console -o 6-print_string
+dotnet build 6-print_string
+cd 6-print_string
+dotnet run
---
+++ csharp-hello_world/6-print_string/6-print_string.cs
@@ -0,0 +1,10 @@
+﻿﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		string str = "Holberton School";
+		Console.WriteLine ($"{str}{str}{str}\n{str[:9]}");
+		}
+}
