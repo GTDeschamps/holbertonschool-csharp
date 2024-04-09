206c05384ab27e018ee9181d834ca9ebf78a73ad
GTDeschamps <g.deschamps@hotmail.fr>
2024-04-09 15:42:18 +0200

    Task 11: positive or negative

--- csharp-ifelse_loop_methods/0-positive_or_negative/0-positive_or_negative.cs
+++ csharp-ifelse_loop_methods/0-positive_or_negative/0-positive_or_negative.cs
@@ -2,19 +2,20 @@
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Random rndm = new Random();
-        int number = rndm.Next(-10, 10);
-	if (number > 0);
+	static void Main(string[] args)
+	{
+		Random rndm = new Random();
+		int number = rndm.Next(-10, 10);
+
+		if (number > 0);
 		{
 			Console.Writeline ($"{number} is positive")
 		}
-	else if (number < 0);
+		else if (number < 0);
 		{
 			Console.Whriteline ($"{number} is negative")
 		}
-	else
+		else
 		{
 			Console.Whriteline ($"{number} is zero")
 		}
