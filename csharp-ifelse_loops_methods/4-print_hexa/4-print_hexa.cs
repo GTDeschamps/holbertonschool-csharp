1ba2f9436ae9f4a07f819039bb98a45d61307c5d
GTDeschamps <g.deschamps@hotmail.fr>
2024-04-11 11:58:18 +0200

    Task 4: print hexa

--- csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
+++ csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -6,7 +6,7 @@ class Program
 	{
 		for (int number = 0; number < 99; number++)
 		{
-			Console.Write($"{number} = 0x{number.ToString("X")}");
+			Console.Write($"{number} = 0x{number.ToString("x2")}\n");
 		}
 	}
 }
