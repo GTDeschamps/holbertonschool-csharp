a9e49447f4d73e772f9bfdfc776f6fbc2ae3c918
GTDeschamps <g.deschamps@hotmail.fr>
2024-04-11 12:29:10 +0200

    Task 5: print comb

---
+++ csharp-ifelse_loops_methods/5-print_comb.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+dotnet new console -o 5-print_comb
+dotnet build 5-print_comb
+cd 5-print_comb
+dotnet run
---
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int number = 00; number < 100; number++)
+            {
+                if (number < 99)
+                {
+                    Console.Write ("{0:00}, ", number);
+                }
+                else
+                {
+                    Console.WriteLine ("{0}",number);
+                }
+            }
+        }
+    }
+}
