99af7d13a12372fada37812ebe06c7d3a90f80c3
GTDeschamps <g.deschamps@hotmail.fr>
2024-04-11 10:06:12 +0200

    Task 4: correction

--- csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
+++ csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -6,7 +6,7 @@ class Program
 	{
 		for (int number = 0; number < 99; number++)
 		{
-			Console.Write($"{number} = 0X{number.ToString("X2")}");
+			Console.Write($"{number} = 0x{number.ToString("x")}");
 		}
 	}
 }
