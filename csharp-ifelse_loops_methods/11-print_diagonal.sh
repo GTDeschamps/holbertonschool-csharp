777a373c17907de5977a8be18ace378a9bfa1e67
GTDeschamps <g.deschamps@hotmail.fr>
2024-04-12 09:30:37 +0200

    Task 11 - print diagonal

---
+++ csharp-ifelse_loops_methods/11-print_diagonal.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+dotnet new console -o 11-print_diagonal
+dotnet build 11-print_diagonal
+cd 11-print_diagonal
+dotnet run
---
+++ csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        int i, j;
+        for (i = 0; i < length; i++)
+        {
+            for (j= length-i; j < length; j++)
+            Console.Write (" ");
+        Console.WriteLine("\\");
+        }
+        Console.WriteLine();
+    }
+}
+
