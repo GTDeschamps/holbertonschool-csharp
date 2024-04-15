cf61bc3eab6262062e6a6a154bd082f6e750dc2d
GTDeschamps <g.deschamps@hotmail.fr>
2024-04-15 14:25:32 +0200

    Task 0 + readme

---
+++ csharp-arrays_lists_dictionaries/0-print_array.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+dotnet new console -o 0-print_array
+dotnet build 0-print_array
+cd 0-print_array
+dotnet run
---
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,26 @@
+﻿using System;
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size == 0)
+        {
+            Console.WriteLine();
+            return  new int[0];
+        }
+        if (size < 0)
+        {
+            Console.Write ("Size cannot be negative");
+            return null;
+        }
+        int[] result = new int[size];
+        for (int i = 0; i < size; i++)
+        {
+            result[i] = i;
+            Console.Write (result[i] +" ");
+        }
+        Console.WriteLine();
+        return result;
+    }
+}
+
