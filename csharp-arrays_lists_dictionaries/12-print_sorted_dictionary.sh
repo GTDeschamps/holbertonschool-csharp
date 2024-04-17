d1e449cfea9986badf9f40bff9edd64b9cb8ae02
GTDeschamps <g.deschamps@hotmail.fr>
2024-04-17 10:50:24 +0200

    Task 12: Sorted dictionary

---
+++ csharp-arrays_lists_dictionaries/12-print_sorted_dictionary.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+dotnet new console -o 12-print_sorted_dictionary
+dotnet build 12-print_sorted_dictionary
+cd 12-print_sorted_dictionary
+dotnet run
---
+++ csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+
+{
+    public static void PrintSorted(Dictionary<string, string> myDict)
+    {
+        List<string> keys = new List<string>(myDict.Keys);
+        keys.Sort();
+
+        foreach (var key in keys)
+        {
+            Console.WriteLine($"{key}: {myDict[key]}");
+        }
+    }
+}
+
