3f0e88ff8de0386c3395cafdbdf4c2239d2d013b
GTDeschamps <g.deschamps@hotmail.fr>
2024-04-17 11:05:30 +0200

    Task 13: best score

---
+++ csharp-arrays_lists_dictionaries/13-best_score.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+dotnet new console -o 13-best_score
+dotnet build 13-best_score
+cd 13-best_score
+dotnet run
+
---
+++ csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        if (myList.Count == 0)
+        {
+            return "None";
+        }
+
+        int maxScore = int.MinValue;
+        string bestKey = "";
+
+        foreach (var kvp in myList)
+        {
+            if (kvp.Value > maxScore)
+            {
+                maxScore = kvp.Value;
+                bestKey = kvp.Key;
+            }
+        }
+        return bestKey;
+    }
+}
+
