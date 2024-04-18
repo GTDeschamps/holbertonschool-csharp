9a4103df4be962cadcd225148ecf8848ffc1a522
GTDeschamps <g.deschamps@hotmail.fr>
2024-04-18 12:29:33 +0200

    Task 6: Linkedlist Length

---
+++ csharp-hashset_stack_queue_linkedlist/6-linkedlist_length.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+dotnet new console -o 6-linkedlist_length
+dotnet build 6-linkedlist_length
+cd 6-linkedlist_length
+dotnet run
---
+++ csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Length(LinkedList<int> myLList)
+    {
+        int length = 0;
+
+         LinkedListNode<int> currentNode = myLList.First;
+         while (currentNode != null)
+         {
+            length++;
+            currentNode = currentNode.Next;
+         }
+         return length;
+    }
+}
+
