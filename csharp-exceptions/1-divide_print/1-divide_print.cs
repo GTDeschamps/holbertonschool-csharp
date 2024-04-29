703337402ae5118d1b5d3fb2e601f2bebf581d43
GTDeschamps <g.deschamps@hotmail.fr>
2024-04-29 14:33:54 +0200

    Task 1: Division exception

--- csharp-exceptions/1-divide_print/1-divide_print.cs
+++ csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -6,12 +6,12 @@ class Int
     {
         try
         {
-            int result = a / b;
-
+            int result = 0;
         }
         catch (DivideByZeroException)
         {
             Console.WriteLine("Cannot divide by zero");
+            result = 0;
         }
         finally
         {
