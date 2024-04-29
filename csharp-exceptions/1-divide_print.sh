31c80f849c1b91b6290d5422a11e2a802e2434e9
GTDeschamps <g.deschamps@hotmail.fr>
2024-04-29 12:51:20 +0200

    Task 1: Division exception

---
+++ csharp-exceptions/1-divide_print.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+dotnet new console -o 1-divide_print
+dotnet build 1-divide_print
+cd 1-divide_print
+dotnet run
---
+++ csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Int
+{
+    public static void divide(int a, int b)
+    {
+        try
+        {
+            int result = a / b;
+            Console.WriteLine($"{a} / {b} = {result}");
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Cannot divide by zero");
+            Console.WriteLine($"{a} / 0 = 0");
+        }
+        finally
+        {
+        }
+    }
+}
+
