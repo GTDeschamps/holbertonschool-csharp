24d3bc4c492d545bb6b53ff449e55f90b99f879d
GTDeschamps <g.deschamps@hotmail.fr>
2024-04-29 14:57:37 +0200

    Task 2: Divided lists

---
+++ csharp-exceptions/2-divide_lists.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+dotnet new console -o 2-divide_lists
+dotnet build 2-divide_lists
+cd 2-divide_lists
+dotnet run
---
+++ csharp-exceptions/2-divide_lists/2-divide_lists.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> Divide(List<int> list1, List<int> list2, int listLength)
+    {
+    List<int> resultList = new List<int>();
+        try
+        {
+            for (int i = 0; i < listLength; i++)
+            {
+                // Check if index is out of range for either list
+                if (i >= list1.Count || i >= list2.Count)
+                {
+                    Console.WriteLine("Out of range");
+                    break;
+                }
+
+                // Divide element by element, handle division by zero
+                try
+                {
+                    int result = list2[i] != 0 ? list1[i] / list2[i] : 0;
+                    resultList.Add(result);
+                }
+                catch (DivideByZeroException)
+                {
+                    Console.WriteLine("Cannot divide by zero");
+                    resultList.Add(0);
+                }
+            }
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+            Console.WriteLine("Out of range");
+        }
+
+        return resultList;
+    }
+}
