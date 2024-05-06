8e83b892f618c527b85770e9259e9d66e0346bf0
GTDeschamps <g.deschamps@hotmail.fr>
2024-05-06 19:33:33 +0200

    Task 5: print the result

--- csharp-classes/5-enemy/5-enemy.cs
+++ csharp-classes/5-enemy/5-enemy.cs
@@ -1,12 +1,40 @@
 ﻿using System;
 
-namespace _5_enemy
+namespace Enemies
 {
-    class Program
+    class Zombie
     {
-        static void Main(string[] args)
+        private int health;
+        private string name = "(No name)";
+
+        public Zombie()
         {
-            Console.WriteLine("Hello World!");
+            health = 0;
+        }
+
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            health = value;
+        }
+
+        public string Name
+        {
+            set { name = value; }
+            get { return name; }
+        }
+
+        public int GetHealth()
+        {
+            return health;
+        }
+
+        public override string ToString()
+        {
+            return $"Zombie Name: {name} / Total Health: {health}";
         }
     }
 }
