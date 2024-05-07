c5168ac4700f8d627721cae204e422e944f13607
GTDeschamps <g.deschamps@hotmail.fr>
2024-05-07 17:50:15 +0200

    Modify Task 1

--- csharp-tdd/1-divide/MyMath/MyMath.cs
+++ csharp-tdd/1-divide/MyMath/MyMath.cs
@@ -1,40 +1,31 @@
-﻿namespace MyMath
+﻿using System;
+
+namespace MyMath
 {
-    public static class Matrix
+    public class Matrix
     {
         public static int[,] Divide(int[,] matrix, int num)
         {
-            // Check if num is 0
-            if (num == 0)
+            try{
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                {
+                    for (int j = 0; j < matrix.GetLength(1); j++)
+                    {
+                        matrix[i, j] = matrix[i, j] / num;
+                    }
+                }
+
+                return matrix;
+            }
+            catch (DivideByZeroException)
             {
                 Console.WriteLine("Num cannot be 0");
                 return null;
             }
-
-            // Check if matrix is null
-            if (matrix == null)
+            catch (NullReferenceException)
             {
                 return null;
             }
-
-            // Get the dimensions of the matrix
-            int rows = matrix.GetLength(0);
-            int cols = matrix.GetLength(1);
-
-            // Create a new matrix with the same dimensions
-            int[,] result = new int[rows, cols];
-
-            // Divide each element of the matrix by num
-            for (int i = 0; i < rows; i++)
-            {
-                for (int j = 0; j < cols; j++)
-                {
-                    result[i, j] = matrix[i, j] / num;
-                }
-            }
-
-            // Return the new matrix
-            return result;
         }
     }
 }
