e60b40c1a169de942c5341fbd87448fdb502f842
GTDeschamps <g.deschamps@hotmail.fr>
2024-05-07 14:27:16 +0200

    Task 0

--- csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
+++ csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
@@ -1,18 +1,41 @@
 using NUnit.Framework;
+using MyMath;
 
 namespace MyMath.Tests
 {
-    public class Tests
+    [TestFixture]
+    public class OperationsTests
     {
-        [SetUp]
-        public void Setup()
+        [Test]
+        public void Add_WhenCalled_ReturnsSumOfTwoNumbers()
         {
+            // Arrange
+            int a = 5;
+            int b = 3;
+            int expected = 8;
+
+            // Act
+            int result = Operations.Add(a, b);
+
+            // Assert
+            Assert.AreEqual(expected, result);
         }
 
         [Test]
-        public void Test1()
+        public void Add_WithNegativeNumbers_ReturnsCorrectSum()
         {
-            Assert.Pass();
+            // Arrange
+            int a = -7;
+            int b = 4;
+            int expected = -3;
+
+            // Act
+            int result = Operations.Add(a, b);
+
+            // Assert
+            Assert.AreEqual(expected, result);
         }
+
+        // You can add more test cases for different scenarios
     }
 }
