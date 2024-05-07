04cccd0edd7f8a1e2f42278719be5f3944154d70
GTDeschamps <g.deschamps@hotmail.fr>
2024-05-07 16:29:52 +0200

    Code of Task 2 modify

--- csharp-tdd/2-max_int/MyMath/MyMath.cs
+++ csharp-tdd/2-max_int/MyMath/MyMath.cs
@@ -3,16 +3,20 @@ using System.Collections.Generic;
 
 namespace MyMath
 {
-    class Operations
+    public static class Operations
     {
         public static int Max(List<int> nums)
         {
+            // Check if nums is empty
             if (nums.Count == 0)
             {
                 return 0;
             }
 
+            // Initialize max to the first element in the list
             int max = nums[0];
+
+            // Iterate through the list and update max if a larger number is found
             foreach (int num in nums)
             {
                 if (num > max)
@@ -21,6 +25,7 @@ namespace MyMath
                 }
             }
 
+            // Return the max number
             return max;
         }
     }
