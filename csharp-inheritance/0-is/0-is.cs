02414dc4babfbdb2211bbb0bfb740037f0097d4e
GTDeschamps <g.deschamps@hotmail.fr>
2024-05-13 13:33:58 +0200

    documentation of T0

--- csharp-inheritance/0-is/0-is.cs
+++ csharp-inheritance/0-is/0-is.cs
@@ -1,8 +1,13 @@
 ﻿using System;
     class Obj
+      /// <summary>
+    /// Vérifie si l'objet est de type int.
+    /// </summary>
     {
         public static bool IsOfTypeInt(object obj)
+            /// <param name="obj">L'objet à vérifier.</param>
         {
             return obj is int;
+                /// <returns>Retourne true si l'objet est de type int, sinon false.</returns>
         }
     }
