4c731b5cfe42249096d98ed8455edc651cefe5cb
GTDeschamps <g.deschamps@hotmail.fr>
2024-05-14 10:49:30 +0200

    modify code of T0  T1

--- csharp-inheritance/0-is/0-is.cs
+++ csharp-inheritance/0-is/0-is.cs
@@ -1,13 +1,8 @@
 ﻿using System;
-    /// <summary>
-    /// Vérifie si l'objet est de type int.
-    /// </summary>
 class Obj
 {
-    /// <param name="obj">L'objet à vérifier.</param>
     public static bool IsOfTypeInt(object obj)
     {
         return obj is int;
-            /// <returns>Retourne true si l'objet est de type int, sinon false.</returns>
     }
 }
--- csharp-inheritance/1-instance/1-instance.cs
+++ csharp-inheritance/1-instance/1-instance.cs
@@ -1,13 +1,8 @@
 ﻿using System;
-    /// <summary>
-    /// Vérifie si l'objet est de type array.
-    /// </summary>
 class Obj
 {
-    /// <param name="obj">L'objet à vérifier.</param>
     public static bool IsInstanceOfArray(object obj)
     {
         return obj is Array;
-            /// <returns>Retourne true si l'objet est de type Array, sinon false.</returns>
     }
 }
