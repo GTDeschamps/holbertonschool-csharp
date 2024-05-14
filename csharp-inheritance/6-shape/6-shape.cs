88d4fac1077568eed6a5285aad47f3773bbf260e
GTDeschamps <g.deschamps@hotmail.fr>
2024-05-14 11:16:54 +0200

    add doc to Task 6

--- csharp-inheritance/6-shape/6-shape.cs
+++ csharp-inheritance/6-shape/6-shape.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 public class Shape
 {
+    // Base class for shapes
     public virtual int Area()
     {
         // Throw NotImplementedException with the specified message
@@ -16,11 +17,13 @@ public class Rectangle : Shape
     public int Width
     {
         get => width;
+        /// Gets or sets the width of the rectangle.
         set
         {
             if (value <0)
             {
                 throw new ArgumentException("Width must be greater or equal than 0");
+                /// If the value is negative, an ArgumentException is thrown.
             }
             else
             {
@@ -31,11 +34,13 @@ public class Rectangle : Shape
     public int Height
     {
         get => height;
+        /// Gets or sets the height of the rectangle.
         set
         {
             if (value <0)
             {
                 throw new ArgumentException("Height must be greater or equal than 0");
+                /// If the value is negative, an ArgumentException is thrown.
             }
             else
             {
