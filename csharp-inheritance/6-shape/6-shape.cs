d87fe1137dbb1b5d15208ee5c1ec15b1255d53f8
GTDeschamps <g.deschamps@hotmail.fr>
2024-05-14 11:08:10 +0200

    Task 6: rectangle

--- csharp-inheritance/6-shape/6-shape.cs
+++ csharp-inheritance/6-shape/6-shape.cs
@@ -1,12 +1,40 @@
 ﻿using System;
-
-namespace _6_shape
+class Shape
 {
-    class Program
+    public virtual int Area()
     {
-        static void Main(string[] args)
+        // Throw NotImplementedException with the specified message
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
+
+class Rectangle : Shape
+{
+    private int width;
+    private int height;
+
+    public int Width
+    {
+        get => width;
+        set
         {
-            Console.WriteLine("Hello World!");
+            if (value <0)
+            {
+                throw new ArgumentException("Width must be greater or equal than 0");
+            }
+            width = value;
+        }
+    }
+    public int Height
+    {
+        get => height;
+        set
+        {
+            if (value <0)
+            {
+                throw new ArgumentException("Height must be greater or equal than 0");
+            }
+            height = value;
         }
     }
 }
