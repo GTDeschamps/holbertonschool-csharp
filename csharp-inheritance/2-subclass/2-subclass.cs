03969f4291de286652391b71bc7f9233a5dd8fd6
GTDeschamps <g.deschamps@hotmail.fr>
2024-05-14 10:52:00 +0200

    edit doc of T2

--- csharp-inheritance/2-subclass/2-subclass.cs
+++ csharp-inheritance/2-subclass/2-subclass.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-    /// <summary>
-    /// Vérifie si l'objet appartien à une subclass.
-    /// </summary>
 class Obj
 {
-    /// <param name="obj">L'objet à vérifier.</param>
     public static bool IsOnlyASubclass(Type derivedType, Type baseType)
     {
         return baseType.IsAssignableFrom(derivedType) && derivedType != baseType;
