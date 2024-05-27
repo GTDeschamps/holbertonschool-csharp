b387eb3a788dbc46bf135cdf38d3d53633c2a2f6
GTDeschamps <g.deschamps@hotmail.fr>
2024-05-27 11:51:55 +0200

    Methode of magnitude

---
+++ csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class VectorMath
+{
+    public static double Magnitude(double[] vector)
+    {
+        if (vector.length == 2 || vector.length == 3)
+        {
+            double sumOfSquares = 0;
+
+            for (double component : vector)
+            {
+                sumOfSquares += component * component;
+            }
+            double magnitude = Math.sqrt(sumOfSquares);
+            return Math.round(magnitude * 100.0) / 100.0;
+            // Arrondi à la centième près
+        }
+        else
+        {
+            return -1;
+        }
+    }
+}
