854a1b63bcd1635eccfb5551d1d67a5e976e9330
GTDeschamps <g.deschamps@hotmail.fr>
2024-05-27 17:37:24 +0200

    Update Magnitude Methode

--- csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -4,15 +4,15 @@ class VectorMath
 {
     public static double Magnitude(double[] vector)
     {
-        if (vector.length == 2 || vector.length == 3)
+        if (vector.Length == 2 || vector.Length == 3)
         {
             double sumOfSquares = 0;
             foreach (double component in vector)
             {
                 sumOfSquares += component * component;
             }
-            double magnitude = Math.sqrt(sumOfSquares);
-            return Math.round(magnitude, 2);
+            double magnitude = Math.Sqrt(sumOfSquares);
+            return Math.Round(magnitude, 2);
         }
         else
         {
