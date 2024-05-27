59acad96b1bf2ee0e0142b373d62bde7b8e36437
GTDeschamps <g.deschamps@hotmail.fr>
2024-05-27 18:08:36 +0200

    Vector Addition

--- csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
+++ csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -1,28 +1,25 @@
 ﻿using System;
 
-namespace _6_vector_addition
+public class VectorMath
 {
-    class VectorMath
+    public static double[] Add(double[] vector1, double[] vector2)
     {
-        public static double[] Add(double[] vector1, double[] vector2)
+        if ((vector1.Length != 2 && vector1.Length != 3) || (vector2.Length != 2 && vector2.Length != 3))
         {
-            if ((vector1.Length != 2 && vector1.Length != 3) || (vector2.Length != 2 && vector2.Length != 3))
-            {
-                return new double[]{-1};
-            }
-
-            if (vector1.Length != vector2.Length)
-            {
-                return new double[]{-1};
-            }
-
-            double[] result = new double[vector1.Length];
-            for (int i = 0; i < vector1.Length; i++)
-            {
-                result[i] = vector1[i] + vector2[i];
-            }
-
-            return result;
+            return new double[] { -1 };
         }
+
+        if (vector1.Length != vector2.Length)
+        {
+            return new double[] { -1 };
+        }
+
+        double[] result = new double[vector1.Length];
+        for (int i = 0; i < vector1.Length; i++)
+        {
+            result[i] = vector1[i] + vector2[i];
+        }
+
+        return result;
     }
 }
