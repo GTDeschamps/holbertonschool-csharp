243916cfe6d72a828969f1f3cff758562058269f
GTDeschamps <g.deschamps@hotmail.fr>
2024-05-28 10:15:02 +0200

    Vector scalar mul

---
+++ csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class VectorMath
+{
+    public static double[] Multiply(double[] vector, double scalar)
+    {
+        if (vector.Length != 2 && vector.Length != 3)
+        {
+            return new double[] { -1 }; // Return vector containing -1 if not 2D or 3D
+        }
+
+        double[] result = new double[vector.Length];
+        for (int i = 0; i < vector.Length; i++)
+        {
+            result[i] = vector[i] * scalar;
+        }
+
+        return result;
+    }
+}
