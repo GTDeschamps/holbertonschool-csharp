b158af5a550339e08cff253d6510644d57c5dad5
GTDeschamps <g.deschamps@hotmail.fr>
2024-05-28 10:32:07 +0200

    dot product

---
+++ csharp-linear_algebra/12-dot_product/12-dot_product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+    class VectorMath
+    {
+        public static double DotProduct(double[] vector1, double[] vector2)
+  {
+        if ((vector1.Length != 2 || vector2.Length != 2) && (vector1.Length != 3 || vector2.Length != 3))
+        {
+            return -1; // Return -1 if vectors are not both 2D or both 3D
+        }
+
+        if (vector1.Length != vector2.Length)
+        {
+            return -1; // Return -1 if vectors are not the same size
+        }
+
+        double dotProduct = 0;
+        for (int i = 0; i < vector1.Length; i++)
+        {
+            dotProduct += vector1[i] * vector2[i];
+        }
+
+        return dotProduct;
+    }
+}
