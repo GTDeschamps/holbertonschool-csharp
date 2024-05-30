a08dad907ba7b0c259017876e8fe552239923b91
GTDeschamps <g.deschamps@hotmail.fr>
2024-05-30 14:12:48 +0200

    matrix transpose

---
+++ csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class MatrixMath
+{
+    public static double[,] Transpose(double[,] matrix)
+    {
+        if (matrix.Length == 0)
+        {
+            return new double[0, 0];
+        }
+
+        int rowCount = matrix.GetLength(0);
+        int colCount = matrix.GetLength(1);
+        double[,] result = new double[colCount, rowCount];
+
+        for (int i = 0; i < rowCount; i++)
+        {
+            for (int j = 0; j < colCount; j++)
+            {
+                result[j, i] = matrix[i, j];
+            }
+        }
+
+        return result;
+    }
+}
