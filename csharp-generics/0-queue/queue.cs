55ac1b7b7ab2f80f4dadeec4dc2c381a885a49bf
GTDeschamps <g.deschamps@hotmail.fr>
2024-07-22 15:19:31 +0200

    documented code

--- csharp-generics/0-queue/queue.cs
+++ csharp-generics/0-queue/queue.cs
@@ -1,9 +1,17 @@
 using System;
 
+/// <summary>
+/// A generic queue class.
+/// </summary>
+/// <typeparam name="T">The type of elements in the queue.</typeparam>
 class Queue<T>
 {
-	public string CheckType()
-	{
-		return typeof(T).Name;
-	}
+    /// <summary>
+    /// Checks the type of elements in the queue.
+    /// </summary>
+    /// <returns>The name of the type of elements in the queue.</returns>
+    public string CheckType()
+    {
+        return typeof(T).Name;
+    }
 }
