aaef0508208fbdff16e86c0e6deb87425f473e7b
GTDeschamps <g.deschamps@hotmail.fr>
2024-07-23 10:57:49 +0200

    add User interface

---
+++ csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+/// <summary>
+/// Base class
+/// </summary>
+public abstract class Base
+{
+    /// <summary>
+    /// Name Property
+    /// </summary>
+    public string name { get; set; }
+    /// <summary>
+    /// Override TostringMethod
+    /// </summary>
+    /// <returns></returns>
+    public override string ToString()
+    {
+        return $"{name} is a {this.GetType()}";
+    }
+}
+
+/// <summary>
+/// IIteractive interface
+/// </summary>
+public interface IInteractive
+{
+    void Interact();
+}
+
+/// <summary>
+/// Ibreakable interface
+/// </summary>
+public interface IBreakable
+{
+    int durability { get; set; }
+    void Break();
+}
+
+/// <summary>
+/// ICollectible interface
+/// </summary>
+public interface ICollectable
+{
+    bool isCollected { get; set; }
+    void Collect();
+}
+
+/// <summary>
+/// TestObject class
+/// </summary>
+public class TestObject : Base, IInteractive, IBreakable, ICollectable
+{
+    public int durability { get; set; }
+    public bool isCollected { get; set; }
+    public void Interact()
+    {
+        // Implement Interact method
+    }
+
+    public void Break()
+    {
+        // Implement Break method
+    }
+
+    public void Collect()
+    {
+        // Implement Collect method
+    }
+}
