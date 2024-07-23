e5732926be196eb73a5203cf7ae80ac6ee15b8cd
GTDeschamps <g.deschamps@hotmail.fr>
2024-07-23 10:14:08 +0200

    C# interface: Abstract

---
+++ csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,10 @@
+﻿using System;
+public abstract class Base
+{
+    public string Name { get; set; }
+    public override string ToString()
+    {
+        return $"{Name} is a {GetType().Name}"
+    }
+}
+
